8ae4b33357fba7ce89697e2ee43d5a30ef2590a1
Richard Chea <rchea817@gmail.com>
2020-04-09 23:17:10 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -3,7 +3,7 @@ def find_max_value(array)
   i = 0
   max_value = array[0]
   while i < array.length do
-    if max_value > array[i]
+    if array[i] > max_value
       max_value = array[i]
     end
     i += 1
